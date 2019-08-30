c95c3f6e52732fe02a4bf5a43a7779479fc52620
Andy8647 <andy8647lgc@gmail.com>
2019-08-30 19:58:03 -0400

    minor changes

--- src/WorkDay/Monday.java
+++ src/WorkDay/Monday.java
@@ -2,6 +2,7 @@ package WorkDay;
 
 import Workers.Worker;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Monday extends WeekDay {
     /**
@@ -18,5 +19,19 @@ public class Monday extends WeekDay {
             else if (12 < t[0] & t[0] <= 15 & t[1]>= 21) {workersToday.add(w);}
             else if (15 < t[0] & t[0] <= 18 & t[1] >= 23) {workersToday.add(w);}
         }
+        if (workersToday.size() >= jobs.size()) {Collections.shuffle(workersToday);}
+        else {System.out.println("No enough workers today!!!");}
+        for (Worker w : workersToday){
+            if (w.isOpen()) {
+
+            }
+        }
     }
+
+
+
+
+
+
+
 }
