3e8822941d1df6c9143864989c6549a72b67ad4a
Xinchen Cai <xinchen.cai@mail.utoronto.ca>
2019-09-05 13:03:02 -0400

    Add Gui starting file

---
+++ src/System/GuiDemo.java
@@ -0,0 +1,47 @@
+package System;
+
+import javafx.application.Application;
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.logging.*;
+
+/**
+ * Some codes were adapted from the YouTube JavaFX teaching video by thenewboston;
+ * URL: https://bit.ly/2hG0e2q
+ */
+
+
+public class GuiDemo extends Application{
+
+    Button button;
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Work Arrangement System");
+
+        StackPane layout = new StackPane();
+
+        Scene scene = new Scene(layout,500, 500);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+}
