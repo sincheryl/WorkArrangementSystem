dafa82a415e38b17fbea817aa99bccc078ea9cd2
Xinchen Cai <xinchen.cai@mail.utoronto.ca>
2019-09-05 14:28:02 -0400

    add new window when click the button and create the back button for each scene

--- src/System/GuiDemo.java
+++ src/System/GuiDemo.java
@@ -27,7 +27,7 @@ import java.util.logging.*;
 
 public class GuiDemo extends Application{
 
-    Button addEmployeeButton, deleteEmployeeButton, setTimeButton;
+    private Scene addEmployeeScene, deleteEmployeeScene, setTimeScene;
 
     public static void main(String[] args){
         launch(args);
@@ -36,17 +36,21 @@ public class GuiDemo extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+
         /*------------------------------------------------------------------------------------*/
         //set main scene
 
         primaryStage.setTitle("Work Arrangement System");
 
-        addEmployeeButton = new Button();
+        Button addEmployeeButton = new Button();
         addEmployeeButton.setText("Add New Employee");
-        deleteEmployeeButton = new Button();
+        addEmployeeButton.setOnAction(e -> primaryStage.setScene(addEmployeeScene));
+        Button deleteEmployeeButton = new Button();
         deleteEmployeeButton.setText("Delete Employee");
-        setTimeButton = new Button();
+        deleteEmployeeButton.setOnAction(e -> primaryStage.setScene(deleteEmployeeScene));
+        Button setTimeButton = new Button();
         setTimeButton.setText("Set Time");
+        setTimeButton.setOnAction(e -> primaryStage.setScene(setTimeScene));
 
         VBox mainWindow = new VBox(20);
         mainWindow.getChildren().addAll(addEmployeeButton, deleteEmployeeButton, setTimeButton);
@@ -57,5 +61,46 @@ public class GuiDemo extends Application{
         primaryStage.show();
         /*------------------------------------------------------------------------------------*/
 
+
+        /*------------------------------------------------------------------------------------*/
+        //set add employee scene
+
+        VBox addEmployeeWindow = new VBox();
+        addEmployeeScene = new Scene(addEmployeeWindow, 300, 300);
+        Button addEmployeeSceneBackButton = new Button();
+        addEmployeeSceneBackButton.setText("Back");
+        addEmployeeSceneBackButton.setOnAction(e -> primaryStage.setScene(scene));
+        addEmployeeWindow.setAlignment(Pos.CENTER);
+        addEmployeeWindow.getChildren().addAll(addEmployeeSceneBackButton);
+
+        /*------------------------------------------------------------------------------------*/
+
+
+        /*------------------------------------------------------------------------------------*/
+        //set add employee scene
+
+        VBox deleteEmployeeWindow = new VBox();
+        deleteEmployeeScene = new Scene(deleteEmployeeWindow, 300, 300);
+        Button deleteEmployeeSceneBackButton = new Button();
+        deleteEmployeeSceneBackButton.setText("Back");
+        deleteEmployeeSceneBackButton.setOnAction(e -> primaryStage.setScene(scene));
+        deleteEmployeeWindow.setAlignment(Pos.CENTER);
+        deleteEmployeeWindow.getChildren().addAll(deleteEmployeeSceneBackButton);
+
+        /*------------------------------------------------------------------------------------*/
+
+
+        /*------------------------------------------------------------------------------------*/
+        //set add employee scene
+
+        VBox setTimeWindow = new VBox();
+        setTimeScene = new Scene(setTimeWindow, 300, 300);
+        Button setTimeSceneBackButton = new Button();
+        setTimeSceneBackButton.setText("Back");
+        setTimeSceneBackButton.setOnAction(e -> primaryStage.setScene(scene));
+        setTimeWindow.setAlignment(Pos.CENTER);
+        setTimeWindow.getChildren().addAll(setTimeSceneBackButton);
+
+        /*------------------------------------------------------------------------------------*/
     }
 }
